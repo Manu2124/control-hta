cf31f700d9af31b5da84bef83e718a80d5eda57e
Manu2124 <chamorrorua21@gmail.com>
2026-05-14 21:24:14 -0500

    Refactor registrar function and add PDF download feature

--- script.js
+++ script.js
@@ -1,70 +1,68 @@
 let total = 0;
-let normales = 0;
-let altos = 0;
-let bajos = 0;
 
-function registrar(){
+}
 
-let nombre = document.getElementById("nombre").value;
-let sis = parseInt(document.getElementById("sistolica").value);
-let dia = parseInt(document.getElementById("diastolica").value);
+function descargarPDF(){
 
-if(nombre === "" || isNaN(sis) || isNaN(dia)){
-alert("Complete todos los campos");
+if(!ultimoPaciente.nombre){
+alert("Primero registre un paciente");
 return;
 }
 
-let estado = "";
-let clase = "";
-let mensaje = "";
+const { jsPDF } = window.jspdf;
+const doc = new jsPDF();
 
-if(sis >= 140 || dia >= 90){
+// ENCABEZADO
 
-estado = "Hipertensión";
-clase = "alta";
-mensaje = "⚠ Paciente en riesgo hipertensivo";
-altos++;
+doc.setFillColor(0,91,187);
+doc.rect(0,0,220,30,'F');
 
-}else if(sis < 90 || dia < 60){
+// TITULO
 
-estado = "Presión Baja";
-clase = "baja";
-mensaje = "🟠 Paciente con presión baja";
-bajos++;
+doc.setTextColor(255,255,255);
+doc.setFontSize(22);
+doc.text("ControlHTA Comunitario", 20, 18);
 
-}else{
+// SUBTITULO
 
-estado = "Normal";
-clase = "normal";
-mensaje = "✅ Presión arterial estable";
-normales++;
+doc.setFontSize(11);
+doc.text("Reporte de Monitoreo de Presión Arterial", 20, 25);
 
-}
+// CUERPO
 
-total++;
+doc.setTextColor(0,0,0);
 
-document.getElementById("totalPacientes").innerText = total;
-document.getElementById("normales").innerText = normales;
-document.getElementById("altos").innerText = altos;
-document.getElementById("bajos").innerText = bajos;
+doc.setFontSize(14);
+doc.text("Datos del Paciente", 20, 50);
 
-document.getElementById("alerta").innerHTML = mensaje;
+// LINEAS
 
-let fecha = new Date().toLocaleString();
+doc.line(20,53,190,53);
 
-let fila = `
-<tr>
-<td>${nombre}</td>
-<td>${sis}/${dia}</td>
-<td class="${clase}">${estado}</td>
-<td>${fecha}</td>
-</tr>
-`;
+// INFORMACION
 
-document.getElementById("tabla").innerHTML += fila;
+doc.setFontSize(12);
 
-document.getElementById("nombre").value = "";
-document.getElementById("sistolica").value = "";
-document.getElementById("diastolica").value = "";
+doc.text(`Paciente: ${ultimoPaciente.nombre}`, 20, 70);
+doc.text(`Presión Arterial: ${ultimoPaciente.sis}/${ultimoPaciente.dia}`, 20, 85);
+doc.text(`Estado Clínico: ${ultimoPaciente.estado}`, 20, 100);
+doc.text(`Resultado: ${ultimoPaciente.mensaje}`, 20, 115);
+doc.text(`Fecha: ${ultimoPaciente.fecha}`, 20, 130);
+
+// MENSAJE FINAL
+
+doc.setFontSize(11);
+doc.text("Este reporte fue generado por el sistema ControlHTA Comunitario.", 20, 170);
+
+doc.text("Proyecto académico de innovación en salud - UNAD", 20, 180);
+
+// FOOTER
+
+doc.setFillColor(31,41,55);
+doc.rect(0,270,220,30,'F');
+
+// GUARDAR
+
+doc.save(`Reporte_${ultimoPaciente.nombre}.pdf`);
 
 }
