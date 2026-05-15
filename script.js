ed6e2d8e2b2deae83c941905a8624663ff5635a1
Manu2124 <chamorrorua21@gmail.com>
2026-05-14 22:47:02 -0500

    Refactor registrar and descargarPDF functions

--- script.js
+++ script.js
@@ -161,3 +161,31 @@ doc.rect(0,270,220,30,'F');
 doc.save(`Reporte_${paciente.nombre}.pdf`);
 
 }
+
+function abrirHistoria(id){
+
+const paciente = pacientes[id];
+
+document.getElementById("modalHC").style.display = "block";
+
+document.getElementById("datosPaciente").innerHTML = `
+
+<p><strong>Paciente:</strong> ${paciente.nombre}</p>
+
+<p><strong>Presión Arterial:</strong> ${paciente.sis}/${paciente.dia}</p>
+
+<p><strong>Estado:</strong> ${paciente.estado}</p>
+
+<p><strong>Fecha:</strong> ${paciente.fecha}</p>
+
+<hr><br>
+
+`;
+
+}
+
+function cerrarModal(){
+
+document.getElementById("modalHC").style.display = "none";
+
+}
