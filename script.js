28e8b918ae2dc0f1dd37f1820b616b6f921258ab
Manu2124 <chamorrorua21@gmail.com>
2026-05-14 22:16:41 -0500

    Refactor registrar and descargarPDF functions

--- script.js
+++ script.js
@@ -138,7 +138,7 @@ doc.text(`Fecha: ${paciente.fecha}`, 20, 130);
 
 doc.setFontSize(11);
 
-doc.text("Este reporte fue generado por el sistema ControlHTA Comunitario.", 20, 170);
+doc.text("Este reporte fue generado por el sistema ControlHTA Comunitario 2026", 20, 170);
 
 doc.text("Proyecto académico de innovación en salud - UNAD", 20, 180);
 
